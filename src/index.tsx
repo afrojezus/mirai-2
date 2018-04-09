6c107b0dea6356ee7ec1b1af8ef98ef6a948d8bd
afroJ <thoralf21@gmail.com>
2018-04-09 21:01:21 +0200

    converting to ts

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from './store';
+import registerServiceWorker from './registerServiceWorker';
+import Index from './pages/index';
+import './index.css';
+
+const rootElement = document.querySelector('#root');
+if (rootElement) {
+  ReactDOM.render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Index />
+      </ConnectedRouter>
+    </Provider>,
+    rootElement
+  );
+}
+registerServiceWorker();
